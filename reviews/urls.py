61ec1115975a6b89710e6c50f85c46edff9cb205
Kristian Cross <etamin1985@yahoo.com>
2025-01-13 18:18:19 +0000

    Add code to define URL routes for the reviews app

---
+++ reviews/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# URL patterns for the reviews app
+urlpatterns = [
+    path('', views.landing_page, name='landing_page'),  # Home page URL
+    path('review/<int:game_id>/', views.review_page, name='review_page'),  # URL for a game's review page
+]
