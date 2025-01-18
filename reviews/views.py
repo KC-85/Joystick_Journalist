8d27b8967057f1b86cec6a5a0ebb26fdcc1b9f8e
Kristian Cross <etamin1985@yahoo.com>
2025-01-18 23:08:13 +0000

    Modify views.py to include form handling

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Game
+from .models import GameForm, ReviewForm
+
+# Create a new game
+def add_game(request):
+    if request.method == "POST":
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('landing_page')
+
+    else:
+        form = GameForm()
+    return render(request, 'reviews/add_game.html', {'form': form})
+
+# Create a new review
+def add_review(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.game = game
+            review.save()
+            return redirect('review_page', game_id=game.id)
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/add_review.html', {'form': form, 'game': game})
 
 # View for the landing page
 def landing_page(request):
