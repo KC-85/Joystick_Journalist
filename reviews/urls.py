f6158acfba59ca618bb36edd08ec898225b5f0a6
Kristian Cross <etamin1985@yahoo.com>
2025-01-19 19:35:15 +0000

    Update urls.py to be able to access views (add game).

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 # URL patterns for the reviews app
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),  # Home page URL
+    path('game/add/', views.add_game, name='add_game'),
     path('review/<int:game_id>/', views.review_page, name='review_page'),  # URL for a game's review page
 ]
