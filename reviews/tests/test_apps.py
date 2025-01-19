1f7257fa7b94732c0f44bc7c38da7616ca6102c3
Kristian Cross <etamin1985@yahoo.com>
2025-01-19 23:58:09 +0000

    Create logic to test app configuration - ensuring the Django app is correctly configured

---
+++ reviews/tests/test_apps.py
@@ -0,0 +1,11 @@
+from django.apps import apps
+from django.test import TestCase
+from reviews.apps import ReviewsConfig
+
+class ReviewsConfigTest(TestCase):
+    """Test case for app configuration"""
+
+    def test_app_name(self):
+        """Check if app is configured correctly"""
+        self.assertEqual(ReviewsConfig.name, 'reviews')
+        self.assertEqual(apps.get_app_config('reviews').name, 'reviews')
