dd334fd09ec88d53f1a63bd8134e33f9339be726
Kristian Cross <etamin1985@yahoo.com>
2025-01-19 23:45:11 +0000

    Create logic for testing that Game and Review models are registered in the Django Admin panel.

---
+++ reviews/tests/test_admin.py
@@ -0,0 +1,14 @@
+from django.contrib.admin.sites import site
+from django.test import TestCase
+from reviews.models import Game, Review
+
+class AdminSiteTest(TestCase):
+    """Test case for Django Admin Site"""
+
+    def test_game_registered(self):
+        """Test if Game model is registered in Admin"""
+        self.assertTrue(site.is_registered(Game))
+
+    def test_reviewed_registered(self):
+        """Test if Review model is registered in Admin"""
+        self.assertTrue(site.is_registered(Review))
