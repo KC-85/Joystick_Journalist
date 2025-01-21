84a45e518062a4b1cba7e9e440fee155d3138b7f
Kristian Cross <etamin1985@yahoo.com>
2025-01-21 21:28:36 +0000

    Fix line length error (review/urls.py)

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),  # Home page URL
     path('game/add/', views.add_game, name='add_game'),
-    path('review/<int:game_id>/', views.review_page, name='review_page'),  # URL for a game's review page
+    path(
+        'review/<int:game_id>/',
+        views.review_page,
+        name='review_page'
+    ),  # URL for a game's review page
 ]
