90d8365636dfdde99025a3142b714ff6cd34f6a6
Kristian Cross <etamin1985@yahoo.com>
2025-01-21 20:25:54 +0000

    Fix blank line error and line length error

--- reviews/apps.py
+++ reviews/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 # Configuration class for the reviews app
+
+
 class ReviewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'  # Default type for primary keys
+    # Default type for primary keys
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'  # Name of the app
