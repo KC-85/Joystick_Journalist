958d252be660ba73528c79462b06686076711c06
Kristian Cross <etamin1985@yahoo.com>
2025-01-21 20:15:44 +0000

    Fix whitespace error before comment on Line 6

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('reviews.urls')), # Include URLs from the reviews app
+    path('', include('reviews.urls')),  # Include URLs from the reviews app
 ]
