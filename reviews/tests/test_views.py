4cc6e587bb365442dcb5c94b7e0552be78f5d92d
Kristian Cross <etamin1985@yahoo.com>
2025-01-22 01:11:44 +0000

    Fix syntax error and blank line error

--- reviews/tests/test_views.py
+++ reviews/tests/test_views.py
@@ -2,12 +2,17 @@ from django.urls import reverse
 from django.test import TestCase
 from reviews.models import Game
 
+
 class LandingPageViewTest(TestCase):
     """Test case for the Landing Page View"""
 
     def setUp(self):
         """Create game instances"""
-        Game.objects.create(title="Super Metroid", release_year=1994, genre="Action")
+        Game.objects.create(
+            title="Super Metroid",
+            release_year=1994,
+            genre="Action"
+        )
 
     def test_landing_page_status_code(self):
         """Check if the landing page loads correctly"""
@@ -15,11 +20,17 @@ class LandingPageViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'reviews/landing_page.html')
 
+
 class ReviewPageViewTest(TestCase):
     """Test case for the Review Page View"""
 
     def setUp(self):
-        self.game = Game.objects.create(title="Killer Instinct", release_year=1994, genre="Fighting")
+        """Create a game instance for the review page"""
+        self.game = Game.objects.create(
+            title="Killer Instinct",
+            release_year=1994,
+            genre="Fighting"
+        )
 
     def test_review_page_status_code(self):
         """Check if the review page loads correctly"""
