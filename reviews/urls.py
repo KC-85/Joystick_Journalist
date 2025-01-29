78268b3d63c98588b1aa41ce23fa6821d2eddc5c
Kristian Cross <etamin1985@yahoo.com>
2025-01-29 12:14:04 +0000

    Remove url path and fix views.py foreignkey

--- reviews/urls.py
+++ reviews/urls.py
@@ -10,10 +10,6 @@ urlpatterns = [
         views.review_page,
         name='review_page'
     ),  # View reviews for a game
-    path(
-        'review/<int:game_id>/add/',
-        views.add_review,
-        name='add_review'
-    ),  # Add a review for a game
+   
     path('reviews/', views.all_reviews, name='all_reviews'),
 ]
