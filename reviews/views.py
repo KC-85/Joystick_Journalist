a8bc40c5c8d498f4daf80b12647044d78f22f78a
Kristian Cross <etamin1985@yahoo.com>
2025-01-29 15:27:58 +0000

    Add CRUD functionality to views.py

--- reviews/views.py
+++ reviews/views.py
@@ -2,41 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Game, Review
 from .forms import GameForm, ReviewForm
 
-
-"""View for the landing page"""
 def landing_page(request):
-    """Fetch all games from the database and render landing page."""
     games = Game.objects.all()
-    return render(
-        request, 'reviews/landing_page.html', {'games': games}
-    )
+    return render(request, 'reviews/landing_page.html', {'games': games})
 
-
-"""View for the review page (includes review form)"""
 def review_page(request, game_id):
-    """Fetch game, display reviews, and handle review submissions."""
     game = get_object_or_404(Game, id=game_id)
     reviews = game.reviews.all()
+    return render(request, 'reviews/review_page.html', {'game': game, 'reviews': reviews})
 
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.game = game
-            review.save()
-            return redirect('review_page', game_id=game.id)  # Redirect to refresh the page
-    else:
-        form = ReviewForm()
-
-    return render(
-        request, 'reviews/review_page.html',
-        {'game': game, 'reviews': reviews, 'form': form}
-    )
-
-
-"""View to add a new game"""
 def add_game(request):
-    """Handle form submission for adding a new game."""
     if request.method == "POST":
         form = GameForm(request.POST)
         if form.is_valid():
@@ -44,17 +19,54 @@ def add_game(request):
             return redirect('landing_page')
     else:
         form = GameForm()
-    
-    return render(
-        request, 'reviews/add_game.html', {'form': form}
-    )
+    return render(request, 'reviews/add_game.html', {'form': form})
 
+def edit_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    if request.method == "POST":
+        form = GameForm(request.POST, instance=game)
+        if form.is_valid():
+            form.save()
+            return redirect('landing_page')
+    else:
+        form = GameForm(instance=game)
+    return render(request, 'reviews/edit_game.html', {'form': form, 'game': game})
 
-"""View to display all reviews across all games"""
-def all_reviews(request):
-    """Fetch all reviews from the database and render the page."""
-    reviews = Review.objects.all()
-    return render(
-        request, 'reviews/all_reviews.html',
-        {'reviews': reviews}
-    )
+def delete_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    if request.method == "POST":
+        game.delete()
+        return redirect('landing_page')
+    return render(request, 'reviews/delete_game.html', {'game': game})
+
+def add_review(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.game = game
+            review.save()
+            return redirect('review_page', game_id=game.id)
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/add_review.html', {'form': form, 'game': game})
+
+def edit_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('review_page', game_id=review.game.id)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'reviews/edit_review.html', {'form': form, 'review': review})
+
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.method == "POST":
+        game_id = review.game.id
+        review.delete()
+        return redirect('review_page', game_id=game_id)
+    return render(request, 'reviews/delete_review.html', {'review': review})
