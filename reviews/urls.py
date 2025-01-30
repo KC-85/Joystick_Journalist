876c9acd095a9ef40b0da63d0f8e42f1d65d7ca5
Kristian Cross <etamin1985@yahoo.com>
2025-01-30 20:11:45 +0000

    Add auth views import

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from .views import SecureLoginView, SecureLogoutView, register
 
 
@@ -7,6 +8,10 @@ urlpatterns = [
     path('login/', SecureLoginView.as_view(), name='login'),
     path('logout/', SecureLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.landing_page, name='landing_page'),
     path('game/add/', views.add_game, name='add_game'),
     path('game/edit/<int:game_id>/', views.edit_game, name='edit_game'),
