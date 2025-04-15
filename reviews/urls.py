dcdc8dadadb178c88df33fd075dfb52862a8ce1a
KC-85 <etamin1985@yahoo.com>
2025-04-15 15:25:43 +0100

    Remove duplicate url patterns

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,12 +6,8 @@ import reviews.views as views
 
 urlpatterns = [
     path('account/login/', SecureLoginView.as_view(), name='login'),
-    path('logout/', SecureLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('account/logout/', SecureLogoutView.as_view(), name='logout'),
+    path('account/register/', register, name='register'),
 
     # ✅ Game URLs
     path('', views.landing_page, name='landing_page'),
@@ -21,7 +17,6 @@ urlpatterns = [
 
     # ✅ Review URLs
     path('review/<int:game_id>/add/', views.review_form, name='add_review'),
-    path('review/<int:game_id>/', views.review_form, name='review_page'),
     path('review/edit/<int:review_id>/', views.edit_review, name='edit_review'),
     path('review/delete/<int:review_id>/', views.delete_review, name='delete_review'),
     path('reviews/', views.all_reviews, name='all_reviews')
