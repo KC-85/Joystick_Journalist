010fd1a0d93bbe036536c607474ce412fe616346
KC-85 <etamin1985@yahoo.com>
2025-04-15 15:54:52 +0100

    Add get user model from django contrib auth,  add username as a foreign key

--- reviews/tests/test_models.py
+++ reviews/tests/test_models.py
@@ -1,44 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from reviews.models import Game, Review, Genre
 
+User = get_user_model()
 
-class GameModelTest(TestCase):
-    """Test case for the Game model"""
+class ReviewModelTest(TestCase):
+    """Test case for the Review model"""
 
     def setUp(self):
-        """Create a Genre and Game instance for testing"""
-        self.genre = Genre.objects.create(name="Action")
+        """Create test data for Genre, Game, User, and Review"""
+        self.genre = Genre.objects.create(name="Racing")
         self.game = Game.objects.create(
             title="Super Metroid",
             release_year=1994,
             genre=self.genre
         )
 
-    def test_game_creation(self):
-        """Test that the game is created correctly"""
-        self.assertEqual(str(self.game), "Super Metroid")
+        # ✅ Create a user instance
+        self.user = User.objects.create_user(username="johnsmith", password="testpass123")
 
-
-class ReviewModelTest(TestCase):
-    """Test case for the Review model"""
-
-    def setUp(self):
-        """Create a Genre, Game, and Review instance"""
-        self.genre = Genre.objects.create(name="Racing")
-        self.game = Game.objects.create(
-            title="Super Mario Kart",
-            release_year=1992,
-            genre=self.genre
-        )
-
+        # ✅ Assign user to reviewer_name
         self.review = Review.objects.create(
             game=self.game,
-            reviewer_name="John Smith",
+            reviewer_name=self.user,
             rating=5,
             comment="Amazing game!"
         )
 
     def test_review_creation(self):
         """Test review creation and ForeignKey relationship"""
-        self.assertEqual(str(self.review), "John Smith - Super Mario Kart")
+        self.assertEqual(str(self.review), "johnsmith - Super Metroid")
         self.assertEqual(self.review.game, self.game)
