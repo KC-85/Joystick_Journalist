f65323ab5d8eda3f3a7f2bc7e9e6a270f510ec8f
Kristian Cross <etamin1985@yahoo.com>
2025-12-22 21:48:22 +0000

    Fix pep8 W291 & E128 errors

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from .views import SecureLoginView, SecureLogoutView, register
 import reviews.views as views
 
+
 urlpatterns = [
     path('account/login/', SecureLoginView.as_view(), name='login'),
     path('account/logout/', SecureLogoutView.as_view(), name='logout'),
@@ -12,16 +13,28 @@ urlpatterns = [
     # ✅ Game URLs
     path('', views.landing_page, name='landing_page'),
     path('game/add/', views.game_form, name='game_form'),
-    path('game/edit/<int:game_id>/', views.game_form, name='edit_game'),
-    path('game/delete/<int:game_id>/', views.delete_game, name='delete_game'),
+    path(
+        'game/edit/<int:game_id>/',
+        views.game_form,
+        name='edit_game'
+    ),
+    path(
+        'game/delete/<int:game_id>/', 
+        views.delete_game, 
+        name='delete_game'),
 
     # ✅ Review URLs
-    path('review/<int:game_id>/add/', views.review_form, name='add_review'),
-    path('review/edit/<int:review_id>/', 
+    path('review/<int:game_id>/add/', 
+        views.review_form, 
+        name='add_review'),
+    path(
+        'review/edit/<int:review_id>/', 
         views.edit_review, 
         name='edit_review'),
-    path('review/delete/<int:review_id>/', 
-        views.delete_review, 
-        name='delete_review'),
-    path('reviews/', views.all_reviews, name='all_reviews')
+    path(
+        'review/delete/<int:review_id>/',
+        views.delete_review,
+        name='delete_review'
+    ),
+    path('reviews/', views.all_reviews, name='all_reviews'),
 ]
